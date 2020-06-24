9707368bb82c6d8d69270db8b0f81b66f29b494d
David Elì <davidelimg1@gmail.com>
2020-06-24 17:08:32 -0500

    Added back buttton to weather display template

--- src/js/menu-utils.js
+++ src/js/menu-utils.js
@@ -2,9 +2,11 @@ const menuUtil = (() => {
   const menuBox = document.getElementById('menu-box');
   const weatherBox = document.getElementById('weather-box');
   const startBtn = document.getElementById('start-btn');
+  const backBox = document.getElementById('back-box');
 
   const start = () => {
     weatherBox.style.display = 'none';
+    backBox.style.display = 'none';
     setTimeout(() => {
       menuBox.classList.remove('hide');
     }, 250);
@@ -13,6 +15,7 @@ const menuUtil = (() => {
       setTimeout(() => {
         menuBox.style.display = 'none';
         weatherBox.style.display = 'block';
+        backBox.style.display = '';
         setTimeout(() => {
           weatherBox.classList.add('show');
         }, 250);
