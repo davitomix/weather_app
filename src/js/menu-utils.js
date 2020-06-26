be687ca22e26b91118bb5b7f613f6b56ce557d9c
David Elì <davidelimg1@gmail.com>
2020-06-26 15:26:51 -0500

    Refactored transitions to promises

--- src/js/menu-utils.js
+++ src/js/menu-utils.js
@@ -6,53 +6,68 @@ const MenUtil = (() => {
   const backBox = document.getElementById('back-box');
   const backBtn = document.getElementById('back-btn');
 
-  const start = () => {
-    weatherBox.style.display = 'none';
-    weatherContBox.style.display = 'none';
-    backBox.style.display = 'none';
-    setTimeout(() => {
-      menuBox.classList.remove('hide');
-      menuBox.addEventListener('transitionend', (e) => {
-        e.preventDefault();
-        startBtn.addEventListener('click', (e) => {
-          e.preventDefault();
-          menuBox.classList.add('hide');
-          setTimeout(() => {
-            menuBox.style.display = 'none';
-            weatherBox.style.display = 'flex';
-            backBox.style.display = 'flex';
-            setTimeout(() => {
-              weatherBox.classList.add('show');
-              backBox.classList.add('show');
-              setTimeout(() => {
-                weatherContBox.style.display = 'flex';
-                setTimeout(() => {
-                  weatherContBox.classList.add('show');
-                  setTimeout(() => {
-                    backBtn.addEventListener('click', (a) => {
-                      a.preventDefault();
-                      weatherContBox.classList.remove('show');
-                      backBox.classList.remove('show');
-                      weatherBox.classList.remove('show');
-                      setTimeout(() => {
-                        weatherBox.style.display = 'none';
-                        weatherContBox.style.display = 'none';
-                        backBox.style.display = 'none';
-                        menuBox.style.display = 'flex';
-                        setTimeout(() => {
-                          menuBox.classList.remove('hide');
-                        }, 100);
-                      }, 1000);
-                    }, false, { once: true });
-                  }, 100);
-                }, 100);
-              }, 400);
-            }, 100);
-          }, 100);
-        }, false, { once: true });
-      });
-    }, 100);
+  const displayElement = (ele) => {
+    ele.style.display = 'flex';
   };
+
+  const undisplayElement = (ele) => {
+    ele.style.display = 'none';
+  };
+
+  const resolveAfterXms = (time) => {
+    return new Promise(resolve => {
+      setTimeout(function() {
+        resolve();
+      }, time);
+    });
+  }
+
+  const transitionToPromise = (el, property, value) =>
+  new Promise(resolve => {
+    el.style[property] = value;
+    const transitionEnded = e => {
+      if (e.propertyName !== property) return;
+      el.removeEventListener('transitionend', transitionEnded);
+      resolve();
+    }
+    el.addEventListener('transitionend', transitionEnded);
+  });
+
+  const start = async () => {
+    await resolveAfterXms(100);
+    await transitionToPromise(menuBox, 'opacity', '1');
+    startBtn.addEventListener('click', startSeq, false, { once: true });
+  }
+
+  const startSeq = async () => {
+    await transitionToPromise(menuBox, 'opacity', '0');
+    undisplayElement(menuBox);
+    displayElement(weatherBox);
+    displayElement(backBox);
+    await resolveAfterXms(100);
+    weatherBox.classList.add('show');
+    backBox.classList.add('show');
+    await resolveAfterXms(400);
+    displayElement(weatherContBox);
+    await resolveAfterXms(100);
+    weatherContBox.classList.add('show');
+    await resolveAfterXms(100);
+    backBtn.addEventListener('click', endSeq, false, { once: true });
+  }
+
+  const endSeq = async () => {
+    weatherContBox.classList.remove('show');
+    backBox.classList.remove('show');
+    weatherBox.classList.remove('show');
+    await resolveAfterXms(1000);
+    undisplayElement(weatherBox);
+    undisplayElement(weatherContBox);
+    undisplayElement(backBox);
+    displayElement(menuBox);
+    await resolveAfterXms(100);
+    await transitionToPromise(menuBox, 'opacity', '1');
+  }
+
   return {
     start,
   };
